ef6d1e04741e721c3af363420758575636dbdeec
Rohan Plante <rohan.v.plante@gmail.com>
2025-11-18 12:50:55 -0500

    completed day one 2024

---
+++ 2024/dayOne/dayOne.py
@@ -0,0 +1,23 @@
+import os
+
+total = 0
+listOne = []
+listTwo = []
+
+with open(os.path.join(os.path.dirname(__file__), 'list.txt'), 'r') as file:
+    data = file.read()
+
+for line in data.splitlines():
+    line = line.split('   ')
+    listOne.append(int(line[0]))
+    listTwo.append(int(line[1]))
+
+listOne = sorted(listOne)
+listTwo = sorted(listTwo)
+
+for i in range(len(listOne)):
+    total += abs(listOne[i] - listTwo[i])
+
+print(total)
+
+# want to go back and see if I can optimize
