46c4e68a76d848efac381cc0c4392c2b1ec9ca5b
Rohan Plante <rohan.v.plante@gmail.com>
2025-11-18 15:29:58 -0500

    finished day two

--- 2024/dayTwo/dayTwo.py
+++ 2024/dayTwo/dayTwo.py
@@ -1,38 +1,42 @@
 import os
 
-numSafe = 0
-rec = 0
-trend = False
-curr = False
-diff = 0
-safe = []
-unsafe = []
+file_name = os.path.join(os.path.dirname(__file__), 'data.txt')
 
-with open(os.path.join(os.path.dirname(__file__), 'data.txt'), 'r') as file:
-    data = file.read()
+def unpack(file):
+    with open(file, 'r') as reader:
+        for block in reader.read().split("\n\n"):
+            return list(map(lambda val: list(map(int, val.split())), block.splitlines()))
 
-# part one
+data = unpack(file_name)
 
-for line in data.splitlines():
-    rec = 0
-    trend = False
-    line = [int(record) for record in line.split(' ')]
-    while rec < len(line) - 1:
-        curr = line[rec] < line[rec + 1]
-        if rec == 0:
-            trend = line[rec] < line[rec + 1]
-        diff = abs(line[rec] - line[rec + 1])
-        if trend != curr or (diff < 1 or diff > 3):
-            unsafe.append(line)
-            break
-        rec += 1
-        if rec == len(line) - 1:
-            safe.append(line)
+def valid_asc(nums: list[int]) -> bool:
+    return all(1 <= b - a <= 3 for a, b in zip(nums, nums[1:]))
+
+def valid_desc(nums: list[int]) -> bool:
+    return all(1 <= a - b <= 3 for a, b in zip(nums, nums[1:]))
+
+def is_valid(data) -> int:
+    numSafe = 0
+    for line in data:
+        if valid_asc(line) or valid_desc(line):
             numSafe += 1
+    return numSafe  
 
-print(numSafe)
+print(is_valid(data))
 
 # part two - problem dampener
 # if you can remove one level to fix report, then it can be considered safe
-print(safe)
-print(unsafe)
+
+def is_valid_damp(data) -> int:
+    numSafeDamp = 0
+    for line in data:
+        variations = [line[:i] + line[i + 1:] for i, _ in enumerate(line)]
+        counted = 0
+        for var in variations:
+            if (valid_asc(var) or valid_desc(var)) and counted == 0:
+                counted += 1
+                numSafeDamp += 1
+    return numSafeDamp
+
+print(is_valid_damp(data))
+
